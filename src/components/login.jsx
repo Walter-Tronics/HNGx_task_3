18977bcde23d08ba0c41e588eafda6a9117d1061
Walter Tronics <waltertronics02@gmail.com>
2023-09-18 21:38:05 +0100

    add login contents

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,44 @@
+import './login.css'
 
-
-const Login = () =>{
+const Login = () => {
 
 
     return (
         <>
-            <div>This is the login page</div>
+            <div className='overall'>
+                <form className="box">
+                    <h1>Login</h1>
+
+                    <div className="w3-row w3-bottombar w3-border-pink w3-rightbar">
+                        <div className="w3-col user_icon">
+                            <i className="w3-xlarge fa fa-user"></i>
+                        </div>
+
+                        <div className="w3-rest">
+                            <input type="text" placeholder="Username" required />
+                        </div>
+
+                    </div>
+
+                    <br />
+
+                    <div className="w3-row w3-bottombar w3-border-pink w3-rightbar password">
+                        <div className="w3-third pass_icon">
+                            <i className="w3-xlarge fa fa-lock"></i>
+                        </div>
+
+                        <div className="w3-third" id="pass">
+                            <input type="password" placeholder="Password" required/>
+                        </div>
+
+                        <div className="w3-third" id="eye" >
+                            <i className="fas fa-eye" onclick="change()"></i>
+                        </div>
+                    </div>
+
+                    <input type="submit" id="submit" value="Login"/>
+                </form>
+            </div>
         </>
     )
 }
