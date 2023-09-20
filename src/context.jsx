69b8a3550f0b61d8a9e95cd9c423a8fee17ee897
Walter Tronics <waltertronics02@gmail.com>
2023-09-20 04:04:13 +0100

    update context

---
+++ src/context.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { createClient } from '@supabase/supabase-js'
+
+
+const DbaseContext = createContext(undefined);
+
+
+export const DbaseProvider = ({children}) => {
+
+    //Create a single supabase client for interacting with your database
+    const supabase = createClient('https://seqnuhydxtgenbwvlyap.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcW51aHlkeHRnZW5id3ZseWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUwNjcyNTUsImV4cCI6MjAxMDY0MzI1NX0.Dt1KQ9Ri7echHypsQTPjBBJXDpAvVERHKcy4Zg8DIi4');
+    
+
+    return (
+        <DbaseContext.Provider value={{ supabase }}>
+            {children}
+        </DbaseContext.Provider>
+    )
+}
+
+export const useDbase = () => useContext(DbaseContext)
