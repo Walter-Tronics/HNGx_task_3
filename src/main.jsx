6b2789a021aee793c767ec96f56673c4fde9d284
Walter Tronics <waltertronics02@gmail.com>
2023-09-20 04:02:39 +0100

    import context provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { DbaseProvider } from './context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <DbaseProvider>
+        <App />
+      </DbaseProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
