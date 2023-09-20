0b917be92e05db60b82025b605dae0ee27009caa
Walter Tronics <waltertronics02@gmail.com>
2023-09-20 09:37:09 +0100

    add loader content

---
+++ src/components/loader.jsx
@@ -0,0 +1,20 @@
+import './loader.css';
+
+
+const Loader = () => {
+
+    return (
+        <div className='load_cont'>
+            <div className="loader">
+                <div className="mr-freeze"></div>
+                <div className="mr-freeze"></div>
+                <div className="mr-freeze"></div>
+                <div className="mr-freeze"></div>
+                <div className="mr-freeze"></div>
+                <div className="mr-freeze"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
