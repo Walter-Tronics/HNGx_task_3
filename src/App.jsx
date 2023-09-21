2ff41ccad4d5d6137ce0913fa8f1c7340a0cad12
Walter Tronics <waltertronics02@gmail.com>
2023-09-21 18:41:37 +0100

    add a new route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Login from "./components/login";
 import Home from './components/home'
+import Signup from './components/signup'
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/gallery' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </>
   )
